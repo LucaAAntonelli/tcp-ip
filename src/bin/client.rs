43c6e41b65a5c4163560acf33fae940c02121d7e
Luca <lucaan@ethz.ch>
2023-11-05 12:06:29 +0100

    tried implementing variable buffer size, failing

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,19 +1,22 @@
+use std::io;
 use std::io::{Read, Write};
 use std::net::TcpStream;
 use std::str::from_utf8;
-
 fn main() {
     match TcpStream::connect("localhost:3333") {
         Ok(mut stream) => {
             println!("Successfully connected to server in port 3333");
 
-            let msg = b"Hello!";
+            let mut input = String::new();
+            io::stdin().read_line(&mut input).unwrap_or(0);
+
+            let msg = input.as_bytes();
 
             stream.write(msg).unwrap();
-            println!("Sent Hello, awaiting reply...");
+            println!("Sent message, awaiting reply...");
 
-            let mut data = [0 as u8; 6]; // using 6 byte buffer
-            match stream.read_exact(&mut data) {
+            let mut data = [0 as u8]; // using 6 byte buffer
+            match stream.read(&mut data) {
                 Ok(_) => {
                     if &data == msg {
                         println!("Reply is ok!");
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -3,15 +3,18 @@ use std::net::{Shutdown, TcpListener, TcpStream};
 use std::thread;
 
 fn handle_client(mut stream: TcpStream) {
-    let mut data = [0 as u8; 50];
+    println!("handle client");
+    let mut data = [0 as u8]; // using 50 byte buffer
     while match stream.read(&mut data) {
         Ok(size) => {
+            // echo everything!
+            println!("Ok arm");
             stream.write(&data[0..size]).unwrap();
             true
         }
         Err(_) => {
             println!(
-                "Error, terminating connection with {}",
+                "An error occurred, terminating connection with {}",
                 stream.peer_addr().unwrap()
             );
             stream.shutdown(Shutdown::Both).unwrap();
@@ -22,17 +25,23 @@ fn handle_client(mut stream: TcpStream) {
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:3333").unwrap();
+    // accept connections and process them, spawning a new thread for each one
     println!("Server listening on port 3333");
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
                 println!("New connection: {}", stream.peer_addr().unwrap());
-                thread::spawn(move || handle_client(stream));
+                thread::spawn(move || {
+                    // connection succeeded
+                    handle_client(stream)
+                });
             }
             Err(e) => {
-                println!("Error: {e}");
+                println!("Error: {}", e);
+                /* connection failed */
             }
         }
     }
+    // close the socket server
     drop(listener);
 }
